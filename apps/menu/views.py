67afccc47bd49300ade535a42d1f9e71a5eb6ce4
imaheshaher <maheshaher035@gmail.com>
2021-02-02 21:39:30 +0530

    menu api

--- apps/menu/views.py
+++ apps/menu/views.py
@@ -12,22 +12,5 @@ class MenuListing(viewsets.ModelViewSet):
     serializer_class = MenuOfTheDaySerializer
     queryset = Menu.objects.all()
 
-    # def get_permissions(self):
-    #     user = self.request.user 
-    #     permission_classes=[]
-    #     if self.action=='retrive' or self.action=='create':
-    #         permission_classes=[IsAuthenticated]
-    #     return [permission() for permission in permission_classes]
-        
     def perform_create(self,serializer):
         serializer.save(vendor=self.request.user)
-    # def get_permissions(self):
-    #     user = self.request.user
-    #     permission_classes = []
-    #     if self.action == 'create':
-    #         permission_classes = [IsAuthenticated]
-    #     elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
-    #         permission_classes = [IsLoggedInUserOrAdmin,IsAuthenticated]
-    #     elif self.action == 'list' or self.action == 'destroy':
-    #         permission_classes = [AllowAny]
-    #     return [permission() for permission in permission_classes]
